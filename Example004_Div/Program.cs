ea9d1777b5cdad4851e25435e5edb359b95caed1
natadolg <natadolg76@gmail.com>
2023-06-08 14:21:46 +0500

    Example004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+double numberC = numberA / numberB;
+Console.WriteLine(numberC);
